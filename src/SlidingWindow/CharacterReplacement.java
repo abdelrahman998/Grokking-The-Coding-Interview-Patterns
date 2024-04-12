1554989f26b7bc05333dafdde9a2a68d0435229b
abdelrahman998 <a.alesawy98@gmail.com>
2024-04-12 10:09:27 +0200

    add sliding window problem

--- src/SlidingWindow/CharacterReplacement.java
+++ src/SlidingWindow/CharacterReplacement.java
@@ -1,9 +1,38 @@
 package SlidingWindow;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+Problem Statement #
+Given a string with lowercase letters only,
+if you are allowed to replace no more than ‘k’ letters with any letter,
+find the length of the longest substring having the same letters after replacement.
+
+Example 1:
+
+Input: String="aabccbb", k=2
+Output: 5
+Explanation: Replace the two 'c' with 'b' to have a longest repeating substring "bbbbb".
+Example 2:
+
+Input: String="abbcb", k=1
+Output: 4
+Explanation: Replace the 'c' with 'b' to have a longest repeating substring "bbbb".
+Example 3:
+
+Input: String="abccde", k=1
+Output: 3
+Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+ */
+
 public class CharacterReplacement {
+
+    public static void main(String[] args) {
+        System.out.println(findLength("aabccbb", 2));
+    }
 
     public static int findLength(String str, int k) {
         int windowStart = 0;
